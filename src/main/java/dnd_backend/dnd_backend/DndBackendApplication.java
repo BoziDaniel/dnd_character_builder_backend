e5f432b37c02e50ce59fbfedd04270e47ac4f2d8
BoziDaniel <daniel.bozi88@gmail.com>
2020-04-20 16:24:01 +0200

    backend initial

---
+++ src/main/java/dnd_backend/dnd_backend/DndBackendApplication.java
@@ -0,0 +1,13 @@
+package dnd_backend.dnd_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DndBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dnd_backend/dnd_backend/DndBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dnd_backend.dnd_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DndBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
