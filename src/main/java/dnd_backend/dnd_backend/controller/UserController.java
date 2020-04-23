9b75c4ec5d83bc9b2a872b513ad7b2756a422fd7
RolandOver9000 <csosz.roland2@gmail.com>
2020-04-23 13:56:12 +0200

    userController implemented for user specific API endpoints

--- src/main/java/dnd_backend/dnd_backend/controller/UserController.java
+++ src/main/java/dnd_backend/dnd_backend/controller/UserController.java
@@ -1,35 +1,24 @@
 package dnd_backend.dnd_backend.controller;
 
-import dnd_backend.dnd_backend.model.Item;
-import dnd_backend.dnd_backend.service.dao.ItemDao;
+import dnd_backend.dnd_backend.model.User;
 import dnd_backend.dnd_backend.service.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
-public class CharacterBuilderController {
-
-    @Autowired
-    @Qualifier("itemService")
-    private ItemDao itemDao;
+public class UserController {
 
     @Autowired
     @Qualifier("userService")
     private UserDao userDao;
 
-
-
-    @GetMapping("/item/list")
-    public List<Item> items() {
-        return itemDao.getItems();
+    @PostMapping("/user/add")
+    public void addUser(@RequestBody User user) {
+        this.userDao.add(user);
     }
-
-
-
 }
